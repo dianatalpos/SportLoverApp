0acb4fcb795de9d405e471c23c8918a2f4e48407
andrea.talpos <diana.talpos@gmail.com>
2021-06-13 11:06:27 +0300

    Add activities to edit profile

--- App/profile/components/ProfileEditForm/ProfileEditForm.tsx
+++ App/profile/components/ProfileEditForm/ProfileEditForm.tsx
@@ -22,41 +22,50 @@ import { launchImageLibrary, MediaType } from "react-native-image-picker";
 import Toast from 'react-native-toast-message';
 import { ProfileService } from "../../services";
 import * as firebase from "firebase";
+import { activityIcons, ActivityItem, ActivityType, activityTypes } from "../../../event";
 
 const ProfileEditForm = ({ profile, onEdit }) => {
 
-  // const [file, setFile] = useState({
-  //   filePath: {
-  //     data: null,
-  //     uri: null,
-  //   },
-  //   fileData: null,
-  //   fileUri: null,
-  // });
+  const [types, setTypes] = useState([]);
+  const [typesError, setTypesError] = useState('');
+
+  const getIconByType = (type: ActivityType): string => {
+    return activityIcons[type];
+  };
+
+  const onTypeSelect = (type: ActivityType): void => {
+    const isInList = types.includes(type)
+    if (isInList) {
+      const list = types.filter(sport => sport != type)
+      setTypes(list)
+    } else {
+      const list = [
+        ...types,
+        type
+      ]
+      setTypes(list)
+    }
+  };
+
+  const activities = activityTypes.map((activity) => (
+    <ActivityItem
+      key={activity}
+      activity={activity}
+      isSelected={types.includes(activity)}
+      onSelect={onTypeSelect}
+      icon={getIconByType(activity)}
+      isSmallIcons={true}
+    />
+  ));
 
   const [galleryPermission, setGalleryPermission] = useState(null);
   const [imageUri, setImageUri] = useState(null);
-  const [imagePath, setImagePath] = useState('');
+  const [imagePath, setImagePath] = useState((profile ? profile.image : null));
 
   const [birthDay, setBirthDay] = useState(
     profile ? (profile.birthday ? new Date(profile?.birthday) : new Date()) : new Date()
   );
 
-
-  useEffect(() => {
-    permissionFunction().then();
-  }, []);
-
-
-  const permissionFunction = async () => {
-    const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
-
-    setGalleryPermission(imagePermission.status === 'granted');
-
-    if (imagePermission.status !== 'granted') {
-      Toast.show({ type: 'error', text1: 'Permission for media access needed.' });
-    }
-  };
 
   const schema = Yup.object().shape({
     firstName: Yup.string()
@@ -66,18 +75,12 @@ const ProfileEditForm = ({ profile, onEdit }) => {
       .required("Last Name is required!")
       .label("Last Name"),
     image: Yup.string().label("Image"),
-    activities: Yup.array().label("Activities"),
   });
   const noImg =
     "https://img.wattpad.com/2eb226316e86e00511a618c2d4f352029fc20219/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f347a65566a557654543535434c673d3d2d3633342e313538306364613237313331623130653933393935343130373335332e6a7067?s=fit&w=720&h=720";
 
   const onAvatar = async () => {
-    // launchImageLibrary(null, (response: any) => {
-    //   console.log(response, "IMAGE RESPONSE");
-    //   if (response.uri) {
-    //     setFile({ ...file, fileUri: response.uri });
-    //   }
-    // });
+
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [4, 4],
@@ -88,24 +91,18 @@ const ProfileEditForm = ({ profile, onEdit }) => {
 
       console.log(profile)
       const profileService = new ProfileService();
-
-      // const response = await profileService.uploadImage(profile.id, result.uri)
-      //       .then((response) => response)
-      //       .catch((error) =>{ 
-      //         console.log(error)
-      //       });
       const response = await uploadImage(result.uri)
-      .then((response) => {
-  
-        console.log("Download URL", response)
-        setImagePath(response);
-        return response;
-      })
-      .catch((error) => {
-        Alert.alert("Upload image failed")
-        console.log(error)
-        return null;
-      })
+        .then((response) => {
+
+          console.log("Download URL", response)
+          setImagePath(response);
+          return response;
+        })
+        .catch((error) => {
+          Alert.alert("Upload image failed")
+          console.log(error)
+          return null;
+        })
     };
   }
 
@@ -121,99 +118,121 @@ const ProfileEditForm = ({ profile, onEdit }) => {
   }
 
   const handleSubmit = (values) => {
+    const typesError = types.length == 0 ? "Please select at least one sport!" : '';
+    setTypesError(typesError);
+    const isValid = !typesError;
+
+    if (!isValid) {
+      return;
+    }
+
     const profile = new Profile();
     profile.firstName = values.firstName;
     profile.lastName = values.lastName;
     profile.birthday = moment(birthDay).format("YYYY-MM-DD");
     profile.shortDescription = values.shortDescription;
     profile.image = imagePath;
-    profile.activities = values.activities;
+    profile.activities = types;
     onEdit(profile);
   };
 
   const imgUri = imageUri || (profile ? profile.image : null);
 
   return (
-    <SafeAreaView style={styles.view}>
-      <TouchableOpacity onPress={onAvatar}>
-        <Image source={{ uri: imgUri }} style={styles.avatar}></Image>
-      </TouchableOpacity>
-      <Formik
-        initialValues={{
-          firstName: profile?.firstName || "",
-          lastName: profile?.lastName || "",
-          activities: profile?.activities || [],
-          image: profile?.image || noImg,
-          shortDescription: profile?.shortDescription || "",
-        }}
-        onSubmit={(values) => handleSubmit(values)}
-        validationSchema={schema}
-      >
-        {({ handleChange, handleSubmit, values, touched, errors }) => (
-          <ScrollView style={styles.form}>
-            <View>
-              <TextInput
-                style={styles.input}
-                placeholder="First Name"
-                value={values.firstName}
-                onChangeText={handleChange("firstName")}
-              />
-              {errors.firstName && touched.firstName ? (
-                <Text style={styles.error}>{errors.firstName}</Text>
-              ) : null}
-            </View>
+    <SafeAreaView style={styles.view} >
+      <ScrollView contentContainerStyle={styles.view} >
+        <TouchableOpacity onPress={onAvatar}>
+          <Image source={{ uri: imgUri }} style={styles.avatar}></Image>
+        </TouchableOpacity>
+        <View style={styles.grid}>{activities}
+          {typesError ? (
+            <Text style={styles.error}>
+              {typesError}
+            </Text>
+          ) : null}
+        </View>
+        <Formik
+          initialValues={{
+            firstName: profile?.firstName || "",
+            lastName: profile?.lastName || "",
+            activities: profile?.activities || [],
+            image: profile?.image || noImg,
+            shortDescription: profile?.shortDescription || "",
+          }}
+          onSubmit={(values) => handleSubmit(values)}
+          validationSchema={schema}
+        >
+          {({ handleChange, handleSubmit, values, touched, errors }) => (
+            <ScrollView style={styles.form}>
+              <View>
+                <TextInput
+                  style={styles.input}
+                  placeholder="First Name"
+                  value={values.firstName}
+                  onChangeText={handleChange("firstName")}
+                />
+                {errors.firstName && touched.firstName ? (
+                  <Text style={styles.error}>{errors.firstName}</Text>
+                ) : null}
+              </View>
 
-            <View>
-              <TextInput
-                style={styles.input}
-                placeholder="Last Name"
-                value={values.lastName}
-                onChangeText={handleChange("lastName")}
-              />
-              {errors.lastName && touched.lastName ? (
-                <Text style={styles.error}>{errors.lastName}</Text>
-              ) : null}
-            </View>
+              <View>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Last Name"
+                  value={values.lastName}
+                  onChangeText={handleChange("lastName")}
+                />
+                {errors.lastName && touched.lastName ? (
+                  <Text style={styles.error}>{errors.lastName}</Text>
+                ) : null}
+              </View>
 
-            <View>
-              <TextInput
-                style={styles.description}
-                multiline={true}
-                numberOfLines={5}
-                placeholder="Description"
-                value={values.shortDescription}
-                onChangeText={handleChange("shortDescription")}
-              />
-              {errors.shortDescription && touched.shortDescription ? (
-                <Text style={styles.error}>{errors.shortDescription}</Text>
-              ) : null}
-            </View>
+              <View>
+                <TextInput
+                  style={styles.description}
+                  multiline={true}
+                  numberOfLines={5}
+                  placeholder="Description"
+                  value={values.shortDescription}
+                  onChangeText={handleChange("shortDescription")}
+                />
+                {errors.shortDescription && touched.shortDescription ? (
+                  <Text style={styles.error}>{errors.shortDescription}</Text>
+                ) : null}
+              </View>
 
-            <View>
-              <Datepicker
-                value={birthDay}
-                onChange={setBirthDay}
-                placeholder={"Select birth day"}
-              />
-            </View>
+              <View>
+                <Datepicker
+                  value={birthDay}
+                  onChange={setBirthDay}
+                  placeholder={"Select birth day"}
+                />
+              </View>
 
-            <TouchableHighlight style={styles.button} onPress={handleSubmit}>
-              <Text style={styles.buttonLabel}>Update</Text>
-            </TouchableHighlight>
-          </ScrollView>
-        )}
-      </Formik>
+              <TouchableHighlight style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonLabel}>Update</Text>
+              </TouchableHighlight>
+            </ScrollView>
+          )}
+        </Formik>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginHorizontal: 30,
+  },
   view: {
     justifyContent: "center",
     alignItems: "center",
   },
   form: {
-    margin: 30,
+    margin: 15,
   },
   title: {
     fontSize: 20,
